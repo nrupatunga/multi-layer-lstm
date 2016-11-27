abb821c8443bfebf61f9cb700cdcaa7a0c07a9c2
Nrupatunga <nrupatunga.tunga@gmail.com>
2016-11-27 23:23:54 +0530

    Update network.py

--- lstm/network.py
+++ lstm/network.py
@@ -23,25 +23,57 @@ class LSTM_network:
         self.num_layers = num_layers
         self.input_dim = input_dim
         self.output_dim = output_dim
-        for i in range(self.num_layers):
-            # weights and bias are reused, so initialize lstm_param only once
-            self.lstm_param[i] = LSTM_param(self.input_dim, self.output_dim, self.num_mem_cells)
 
-        for i in range(self.num_layers):
-            self.lstm_node_list[i] = {}
+        if num_layers is 1:
+            self.lstm_param[0] = LSTM_param(self.input_dim, self.output_dim, self.num_mem_cells)
+            self.lstm_node_list[0] = {}
             for j in range(self.seq_len):
                 lstm_state = LSTM_state(self.input_dim, self.output_dim, self.num_mem_cells)
-                self.lstm_node_list[i][j] = LSTM_node(self.lstm_param[i], lstm_state)
+                self.lstm_node_list[0][j] = LSTM_node(self.lstm_param[0], lstm_state)
+        else:
+            # Input layer
+            self.lstm_param[0] = LSTM_param(self.input_dim, self.num_mem_cells, self.num_mem_cells)
+            self.lstm_node_list[0] = {}
+            for j in range(self.seq_len):
+                lstm_state = LSTM_state(self.input_dim, self.num_mem_cells, self.num_mem_cells)
+                self.lstm_node_list[0][j] = LSTM_node(self.lstm_param[0], lstm_state)
+
+            # Hidden layers
+            for i in range(1, self.num_layers - 1):
+                self.lstm_node_list[i] = {}
+                self.lstm_param[i] = LSTM_param(self.num_mem_cells, self.num_mem_cells, self.num_mem_cells)
+                for j in range(self.seq_len):
+                    lstm_state = LSTM_state(self.num_mem_cells, self.num_mem_cells, self.num_mem_cells)
+                    self.lstm_node_list[i][j] = LSTM_node(self.lstm_param[i], lstm_state)
+
+            # Output layer
+            self.lstm_param[self.num_layers - 1] = LSTM_param(self.num_mem_cells, self.output_dim, self.num_mem_cells)
+            self.lstm_node_list[self.num_layers - 1] = {}
+            for j in range(self.seq_len):
+                lstm_state = LSTM_state(self.num_mem_cells, self.output_dim, self.num_mem_cells)
+                self.lstm_node_list[self.num_layers - 1][j] = LSTM_node(self.lstm_param[self.num_layers - 1], lstm_state)
 
         # loss
         self.smooth_loss = -np.log(1.0 / output_dim) * self.seq_len  # loss at iteration 0
 
     def reset(self):
         ''' Reset all the LSTM layer before next batch'''
-        for layer in range(self.num_layers):
+
+        self.lstm_node_list[0] = {}
+        for j in range(self.seq_len):
+            lstm_state = LSTM_state(self.input_dim, self.num_mem_cells, self.num_mem_cells)
+            self.lstm_node_list[0][j] = LSTM_node(self.lstm_param[0], lstm_state)
+
+        for layer in range(1, self.num_layers - 1):
+            self.lstm_node_list[layer] = {}
             for j in range(self.seq_len):
-                lstm_state = LSTM_state(self.input_dim, self.output_dim, self.num_mem_cells)
+                lstm_state = LSTM_state(self.num_mem_cells, self.num_mem_cells, self.num_mem_cells)
                 self.lstm_node_list[layer][j] = LSTM_node(self.lstm_param[layer], lstm_state)
+
+        self.lstm_node_list[self.num_layers - 1] = {}
+        for j in range(self.seq_len):
+            lstm_state = LSTM_state(self.num_mem_cells, self.output_dim, self.num_mem_cells)
+            self.lstm_node_list[self.num_layers - 1][j] = LSTM_node(self.lstm_param[self.num_layers - 1], lstm_state)
 
     def calculate_loss(self, target, num_layers):
         ''' Cross entropy loss'''
@@ -66,32 +98,23 @@ class LSTM_network:
             h_prev - previous hidden state
         '''
 
-        # first layer
-        layer = 0
-        # first iteration at time t = 0
-        if t_idx == 0 and not bool(s_prev) and not bool(h_prev):
-            self.lstm_node_list[layer][t_idx].forward_pass(input_x)
-        # second iteration at time t = 1,... so on
-        elif t_idx == 0 and bool(s_prev) and bool(h_prev):
-            self.lstm_node_list[layer][t_idx].forward_pass(input_x, s_prev[layer], h_prev[layer])
-        else:
-            self.s_prev = self.lstm_node_list[layer][t_idx - 1].state.s
-            self.h_prev = self.lstm_node_list[layer][t_idx - 1].state.h
-            self.lstm_node_list[layer][t_idx].forward_pass(input_x, self.s_prev, self.h_prev)
-
-        # second layer till top
-        for layer in range(1, num_layers):
-            layer_input = self.lstm_node_list[layer - 1][t_idx].state.y
+        # Forward pass
+        layer_input, is_output_layer = input_x, False
+        for layer in range(num_layers):
+            if layer is num_layers - 1:
+                is_output_layer = True
             # first iteration at time t = 0
             if t_idx == 0 and not bool(s_prev) and not bool(h_prev):
-                self.lstm_node_list[layer][t_idx].forward_pass(layer_input)
+                self.lstm_node_list[layer][t_idx].forward_pass(layer_input, is_output_layer=is_output_layer)
             # second iteration at time t = 1,... so on
             elif t_idx == 0 and bool(s_prev) and bool(h_prev):
-                self.lstm_node_list[layer][t_idx].forward_pass(layer_input, s_prev[layer], h_prev[layer])
+                self.lstm_node_list[layer][t_idx].forward_pass(layer_input, s_prev[layer], h_prev[layer], is_output_layer=is_output_layer)
             else:
                 self.s_prev = self.lstm_node_list[layer][t_idx - 1].state.s
                 self.h_prev = self.lstm_node_list[layer][t_idx - 1].state.h
-                self.lstm_node_list[layer][t_idx].forward_pass(layer_input, self.s_prev, self.h_prev)
+                self.lstm_node_list[layer][t_idx].forward_pass(layer_input, self.s_prev, self.h_prev, is_output_layer=is_output_layer)
+
+            layer_input = self.lstm_node_list[layer][t_idx].state.h
 
     def feed_backward(self, target, num_layers):
         ''' Backpropagation
@@ -136,17 +159,19 @@ class LSTM_network:
                 if layer == num_layers - 1:
                     dy = np.copy(self.lstm_node_list[layer][i].state.prob)
                     dy[tt] -= 1
+                    self.lstm_node_list[layer][idx].param.dwy += np.outer(dy, state.h)
+                    self.lstm_node_list[layer][idx].param.dby += dy
+                    dh = np.dot(param.wy.T, dy)
 
-                # gradient till cell state at time t
-                dh = np.dot(param.wy.T, dy) + dh_next[layer]
-                ds = dh[layer] * state.o + ds_next[layer]
-                # ds = dh * state.o * (1 - (np.tanh(state.s) ** 2)) + ds_next
+                dh += dh_next[layer]
+                # ds = dh * state.o + ds_next[layer]
+                ds = dh * state.o * (1 - (np.tanh(state.s) ** 2)) + ds_next[layer]
 
                 # gradients till the non linearities for gates
                 dg = state.i * ds
                 di = state.g * ds
                 df = self.lstm_node_list[layer][i].s_prev * ds
-                do = dh * state.s
+                do = dh * np.tanh(state.s)
 
                 # gradients including non-linearities
                 dg_input = (1.0 - state.g ** 2) * dg
@@ -155,8 +180,6 @@ class LSTM_network:
                 do_input = (1.0 - state.o) * state.o * do
 
                 # Update gradients
-                self.lstm_node_list[layer][idx].param.dwy += np.outer(dy, state.h)
-                self.lstm_node_list[layer][idx].param.dby += dy
                 self.lstm_node_list[layer][idx].param.dwg += np.outer(dg_input, xc)
                 self.lstm_node_list[layer][idx].param.dwi += np.outer(di_input, xc)
                 self.lstm_node_list[layer][idx].param.dwf += np.outer(df_input, xc)
@@ -166,7 +189,7 @@ class LSTM_network:
                 self.lstm_node_list[layer][idx].param.dbf += df_input
                 self.lstm_node_list[layer][idx].param.dbo += do_input
 
-                ds_next[layer] = state.f * ds[layer]
+                ds_next[layer] = state.f * ds
                 # compute bottom diff
                 dxc = np.zeros_like(xc)
                 dxc += np.dot(param.wi.T, di_input)
@@ -174,7 +197,7 @@ class LSTM_network:
                 dxc += np.dot(param.wo.T, do_input)
                 dxc += np.dot(param.wg.T, dg_input)
                 dh_next[layer] = dxc[x_dim:]
-                dy = dxc[:x_dim]
+                dh = dxc[:x_dim]
 
     def apply_grad(self, lr):
         # update your weights
